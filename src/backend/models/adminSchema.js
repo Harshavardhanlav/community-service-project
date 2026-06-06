3554846f1c9a5c7a6b6de77bd9d3ab2d0c2d4a64
Harshavardhan P <hprabandhakavi@gmail.com>
2026-06-06 13:09:52 +0530

    setup admin registration through frontend

--- src/backend/models/adminSchema.js
+++ src/backend/models/adminSchema.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
+    schoolName:{
+        type:String,
+        required:true
+    },
     username:{
         type:String,
         required:true,
         unique:true
+    },
+    email:{
+        type:String,
+        required:true
     },
     password:{
         type:String,
--- src/frontend/AuthPanel.jsx
+++ src/frontend/AuthPanel.jsx
@@ -1,8 +1,38 @@
 import "./AuthPanel.css";
-
+import { useState } from "react";
 import Spline from "@splinetool/react-spline";
+import { School } from "lucide-react";
 
 export function AuthPanel() {
+   const [Username, SetUsername] = useState("");
+   const [Email, SetEmail] = useState("");
+   const [Password, SetPassword] = useState("");
+   const [Schoolname, SetSchool] = useState("");
+const handleRegister = async () => {
+
+   const response = await fetch(
+      "http://localhost:5000/admin/create-admin",
+      {
+         method: "POST",
+
+         headers: {
+            "Content-Type": "application/json"
+         },
+
+         body: JSON.stringify({
+            schoolName: Schoolname,
+            username: Username,
+            email: Email,
+            password: Password
+         })
+      }
+   );
+
+   const data = await response.json();
+
+   console.log(data);
+
+} 
 
    return (
 
@@ -32,33 +62,38 @@ export function AuthPanel() {
 
                   <div className="auth-inputs">
 
-                     <input
-                        type="text"
-                        placeholder="School Name"
-                        className="auth-input"
-                     />
+                    <input
+   type="text"
+   placeholder="School Name"
+   className="auth-input"
+   value={Schoolname}
+   onChange={(e) => SetSchool(e.target.value)}
+/>
 
-                     <input
-                        type="text"
-                        placeholder="Admin Username"
-                        className="auth-input"
-                     />
-
-                     <input
-                        type="email"
-                        placeholder="Admin Email"
-                        className="auth-input"
-                     />
-
-                     <input
-                        type="password"
-                        placeholder="Create Password"
-                        className="auth-input"
-                     />
-
+   <input
+   type="text"
+   placeholder="Admin Username"
+   className="auth-input"
+   value={Username}
+   onChange={(e) => SetUsername(e.target.value)}
+/>
+<input
+   type="email"
+   placeholder="Admin Email"
+   className="auth-input"
+   value={Email}
+   onChange={(e) => SetEmail(e.target.value)}
+/>
+<input
+   type="password"
+   placeholder="Create Password"
+   className="auth-input"
+   value={Password}
+   onChange={(e) => SetPassword(e.target.value)}
+/>
                   </div>
 
-                  <button className="auth-submit-btn">
+                  <button className="auth-submit-btn" onClick={handleRegister}>
                      Create Workspace
                   </button>
 
